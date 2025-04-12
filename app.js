0f64f4331475e0c132012dcdbbd3e2cbbeba6781
AlexandreLeCam <a.lecam@myanapro.com>
2025-04-12 13:26:36 +0200

    api v2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', async () => {
-    const res = await fetch('http://projet4.traefik.me/api.php');
+    const res = await fetch('http://sandbox.projet-4.me/api.php');
     const json = await res.json();
 
     const container = document.getElementById('messages');
@@ -7,7 +7,7 @@ window.addEventListener('DOMContentLoaded', async () => {
     if (json.success && Array.isArray(json.data)) {
         json.data.forEach(item => {
             const div = document.createElement('div');
-            div.textContent = `${item.id} - ${item.message}`;
+            div.textContent = `${item.id} - ${item.content}`;
             container.appendChild(div);
         });
     } else {
